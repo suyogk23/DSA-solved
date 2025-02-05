2a67c3f7a0b6997a93a689f8a002ef6451097098
suyogk23 <suyogk2301@gmail.com>
2025-02-05 21:04:11 +0530

    climbing stairs

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,22 +1,45 @@
 class Solution:
-    #dp: optimized approach
+    #DP: all approaches
     def climbStairs(self, n: int) -> int:
-        arr = [-1 for i in range(n+1)]
-
-        def dp(n):
-            #base cases
-            if n==0:
-                return 1
-            if(arr[n]==-1):
-                if n>=2:
-                    arr[n] = dp(n-2) + dp(n-1)
-                    return arr[n]
-                else:
-                    arr[n] = dp(n-1)
-                    return arr[n]
-            else:
-                return arr[n]
+        #bottom up
+        # arr = [-1 for i in range(n+1)]
+        # def top_down(n):
+        #     #base cases
+        #     if n==0:
+        #         return 1
+        #     if(arr[n]==-1):
+        #         if n>=2:
+        #             arr[n] = top_down(n-2) + top_down(n-1)
+        #             return arr[n]
+        #         else:
+        #             arr[n] = top_down(n-1)
+        #             return arr[n]
+        #     else:
+        #         return arr[n]
         
-        dp(n)
-        return arr[n]
+        # top_down(n)
+        # return arr[n]
+
+        #top down
+        # arr = [0 for i in range(n+1)]
+        # if n<=2:
+        #     return n
+        # arr[1], arr[2] = 1, 2
+        # for i in range (3, n+1):
+        #     arr[i] = arr[i-1]+arr[i-2]
+        # return arr[n]
+
+        #space optimised
+        if n<=2:
+            return n
+        temp = 0
+        prev = 1
+        cur = 2
+        for i in range(3, n+1):
+            temp = cur
+            cur = cur+prev
+            prev = temp
+        return cur
+
+
         
