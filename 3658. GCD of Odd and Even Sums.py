5c8482e37701e6518e8d1482b30376c354499ae0
suyogk23 <suyogk2301@gmail.com>
2025-08-24 22:40:51 +0530

    24 aug contest solution

---
+++ 3658. GCD of Odd and Even Sums.py
@@ -0,0 +1,18 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        if n == 1:
+            return 1
+        sumOdd = 0
+        sumEven = 0
+        for i in range(1, n*2+1):
+            if i%2 == 0:
+                #even
+                sumEven += i
+            else:
+                #odd
+                sumOdd += i
+        return math.gcd(sumOdd, sumEven)
+            
+
+                
+        
