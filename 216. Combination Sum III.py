d84f1e1129a644f744686699b0d0f1cffd6e0515
suyogk23 <suyogk2301@gmail.com>
2025-08-30 23:09:04 +0530

    combination sum 3

---
+++ 216. Combination Sum III.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        # soultion by @suyogk23 GITHUB
+        # recursion from 1 to 9, take current number and not take current number
+        def dfs(i, nums):
+            if i > 9 or len(nums) == k:
+                if sum(nums) == n and len(nums) == k:
+                    ans.append(nums)
+            else:
+                dfs(i+1, nums.copy())
+                nums.append(i)
+                dfs(i+1, nums.copy())
+        ans = []
+        dfs(1, [])
+        return ans
