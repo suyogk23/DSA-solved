653dd2c9408b711c17e355b68126b6d920dca25e
suyogk23 <suyogk2301@gmail.com>
2025-11-02 01:14:45 +0530

    rotten oranges refined

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -1,50 +1,48 @@
 class Solution:
     # solution by @suyogk23 GITHUB
-    # do bfs and track min time in m x n time array and rotten orange source could be multiple
+    # keep track of number of fresh oranges
+    # now append all rotten source in tree and do bfs
+    # if fresh oranges are left after bfs then return -1
+    # else return the time elapsed (number of levels in bfs -> track this in queue)
     def orangesRotting(self, grid: List[List[int]]) -> int:
         # global constants
         m, n = len(grid), len(grid[0])
         rvec = [0, -1, 1, 0]
         cvec = [-1, 0, 0, 1]
-        # rot time information to keep track of min time from multiple source
-        rot_time = [[float('inf') for _ in range(n)] for _ in range(m)]
-        # bfs traversal and time updation
-        def bfs(x, y):
-            vis = set()
-            q = deque([(x, y, 0)])
-            while q:
-                #check nodes each level
-                for i in range(len(q)):
-                    node = q.popleft()
-                    row = node[0]
-                    col = node[1]
-                    time = node[2]
-                    rot_time[row][col] = min(rot_time[row][col], time)
-                    vis.add((row, col))
-                    for i in range(4):
-                        r = row + rvec[i]
-                        c = col + cvec[i]
-                        if r>=0 and c>=0 and r<m and c<n:
-                            if ((r, c) not in vis) and (grid[r][c] == 1):
-                                # update rot time to minimum
-                                q.append((r, c, time+1))
+        q = deque()
+        # keep track of fresh orange count in array
+        fresh = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh += 1
+        # append all the rotten sources in the queue, this way, we can simulate multi source rot from BFS traversal
+        # all sources will have time = 0 initially
+        time = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    bfs(i, j)
-        # check for isolated fresh oranges
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1 and rot_time[i][j] == float('inf'):
-                    return -1
-        # check for max rot time
-        max_rot_time = 0
-        for i in range(m):
-            for j in range(n):
-                if rot_time[i][j] != float('inf'):
-                    max_rot_time = max(max_rot_time, rot_time[i][j])
-        return max_rot_time
+                    q.append((i, j, time))
+        while q:
+            #check nodes each level
+            for i in range(len(q)):
+                node = q.popleft()
+                row, col , time = node[0], node[1], node[2]
+                for i in range(4):
+                    r = row + rvec[i]
+                    c = col + cvec[i]
+                    if r>=0 and c>=0 and r<m and c<n:
+                        if grid[r][c] == 1:
+                            grid[r][c] = 2
+                            fresh -= 1 # this is required to check if it is possible to rot all oranges(no fresh isolated oranges)
+                            q.append((r, c, time + 1))
+        if fresh > 0:
+            return -1 # not possible to rot all oranges
+            
+        return time
+                    
 
 
 
         
+
