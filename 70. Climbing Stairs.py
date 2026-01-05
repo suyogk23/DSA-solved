d9675d011355af3097bab4d17c14ba0a23747d72
suyogk23 <suyogk2301@gmail.com>
2026-01-05 21:52:02 +0530

    climbng stairs

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,45 +1,13 @@
 class Solution:
-    #DP: all approaches
     def climbStairs(self, n: int) -> int:
-        #bottom up
-        # arr = [-1 for i in range(n+1)]
-        # def top_down(n):
-        #     #base cases
-        #     if n==0:
-        #         return 1
-        #     if(arr[n]==-1):
-        #         if n>=2:
-        #             arr[n] = top_down(n-2) + top_down(n-1)
-        #             return arr[n]
-        #         else:
-        #             arr[n] = top_down(n-1)
-        #             return arr[n]
-        #     else:
-        #         return arr[n]
+        # bottom up (tabulation)
+        if n == 1:
+            return 1
         
-        # top_down(n)
-        # return arr[n]
+        c1, c2 = 1, 2
 
-        #top down
-        # arr = [0 for i in range(n+1)]
-        # if n<=2:
-        #     return n
-        # arr[1], arr[2] = 1, 2
-        # for i in range (3, n+1):
-        #     arr[i] = arr[i-1]+arr[i-2]
-        # return arr[n]
-
-        #space optimised
-        if n<=2:
-            return n
-        temp = 0
-        prev = 1
-        cur = 2
-        for i in range(3, n+1):
-            temp = cur
-            cur = cur+prev
-            prev = temp
-        return cur
-
-
-        
+        for i in range(n-2):
+            temp = c2
+            c2 = c1+c2
+            c1 = temp
+        return c2
