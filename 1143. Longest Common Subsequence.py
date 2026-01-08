7db982136361fea084b9e36b9d1780acb25422b1
suyogk23 <suyogk2301@gmail.com>
2026-01-08 18:57:26 +0530

    1143. Longest Common Subsequence modified

--- 1143. Longest Common Subsequence.py
+++ 1143. Longest Common Subsequence.py
@@ -1,21 +1,32 @@
 class Solution:
-    # solution by @suyogk GITHUB
-    # if text1[i] == text2[j]: DP[i][j] = DP[i + 1][j + 1] + 1 
-    # else: DP[i][j] = max(DP[i + 1][j], DP[i][j + 1])
-    # handle base case 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n1, n2 = len(text1), len(text2)
-        dp = {}
-        def dfs(i, j):
-            if i >= n1 or j >= n2:
-                return 0
-            if (i, j) in dp:
-                return dp[(i, j)]
-            if text1[i] == text2[j]:
-                dp[(i, j)] =  1 + dfs(i+1, j+1)
-                return dp[(i, j)]
-            else:
-                dp[(i, j)] = max(dfs(i, j+1), dfs(i+1, j))
-                return dp[(i, j)]
+        # BOTTOM UP
+        n, m = len(text1), len(text2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
         
-        return dfs(0, 0)
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        return dp[0][0]
+
+        # TOP DOWN
+        # dp = [[0 for _ in range(m)] for _ in range(n)]
+        # def dfs(i, j):
+        #     if i == n or j == m:
+        #         return 0
+        #     if dp[i][j] is not None:
+        #         return dp[i][j]
+        #     if text1[i] == text2[j]:
+        #         res = 1 + dfs(i+1, j+1)
+        #     else:
+        #         res = max(dfs(i+1, j), dfs(i, j+1))
+        #     dp[i][j] = res
+        #     return dp[i][j]
+
+        # return dfs(0, 0)
+            
+
