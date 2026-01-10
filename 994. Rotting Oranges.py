767f1e09f927308f183c5676e6d088890f4bde14
suyogk23 <suyogk2301@gmail.com>
2026-01-10 15:30:01 +0530

    rotting oranges modified

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -1,48 +1,36 @@
 class Solution:
-    # solution by @suyogk23 GITHUB
-    # keep track of number of fresh oranges
-    # now append all rotten source in tree and do bfs
-    # if fresh oranges are left after bfs then return -1
-    # else return the time elapsed (number of levels in bfs -> track this in queue)
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # global constants
-        m, n = len(grid), len(grid[0])
-        rvec = [0, -1, 1, 0]
-        cvec = [-1, 0, 0, 1]
-        q = deque()
-        # keep track of fresh orange count in array
-        fresh = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    fresh += 1
-        # append all the rotten sources in the queue, this way, we can simulate multi source rot from BFS traversal
-        # all sources will have time = 0 initially
-        time = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 2:
-                    q.append((i, j, time))
+        # BFS 
+        n, m = len(grid), len(grid[0])
+        rvec, cvec = [-1, 0, 0, 1], [0, -1, 1, 0]
+        vis = [[0 for _ in range(m)] for _ in range(n)]
+        # add all rotten oranges as starting source to Q
+        q = deque([])
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2 and vis[i][j] == 0:
+                    # ans += rot(i, j)
+                    q.append((i, j))
+        # Simulate rotting process, do BFS traversal from the source
+        duration = -1
         while q:
-            #check nodes each level
-            for i in range(len(q)):
+            cur_q_len = len(q)
+            for i in range(cur_q_len):
                 node = q.popleft()
-                row, col , time = node[0], node[1], node[2]
-                for i in range(4):
-                    r = row + rvec[i]
-                    c = col + cvec[i]
-                    if r>=0 and c>=0 and r<m and c<n:
-                        if grid[r][c] == 1:
+                for v in range(4):
+                    r = node[0] + rvec[v]
+                    c = node[1] + cvec[v]
+                    # if there is an orage adj then rot it and mark as visited
+                    if r>=0 and c>=0 and r<n and c<m and grid[r][c]==1:
+                        if vis[r][c] == 0:
+                            vis[r][c] = 1
                             grid[r][c] = 2
-                            fresh -= 1 # this is required to check if it is possible to rot all oranges(no fresh isolated oranges)
-                            q.append((r, c, time + 1))
-        if fresh > 0:
-            return -1 # not possible to rot all oranges
-            
-        return time
-                    
+                            q.append((r, c)) 
+            duration += 1
+        # check for any unrotten oranges
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    return -1
 
-
-
-        
-
+        return duration if duration > 0 else 0
