9d8b092a48cdf1b639b537088e5e273abbf4479f
suyogk23 <suyogk2301@gmail.com>
2026-02-11 00:57:05 +0530

    2398. Maximum Number of Robots Within Budget optimized

--- 2398. Maximum Number of Robots Within Budget.py
+++ 2398. Maximum Number of Robots Within Budget.py
@@ -1,37 +1,31 @@
 class Solution:
     def maximumRobots(self, chargeTimes: List[int], runningCosts: List[int], budget: int) -> int:
-        # maintain a sliding window, traverste left to right, expand and shrink it as the budget increase
-        # use hashmap to trach occurance of element and heap to get max element
-        # TC: O(n log n) n for linear traversal and log n for heap operations (push and pop)
+        # TC: O(n)
         # Space: O(n)
-        n = len(chargeTimes)
-        ans = 0
-        # max(chargeTimes) + k * sum(runningCosts)
-        hm = defaultdict(int)
-        pq = []
-        cost = 0
-        cur_max = 0
+        # slididng window to track maximum subarray
+        # use a monotonic decreasting queue: [3, 2, 1] to track next maximum chargeTimes
+        # this way we store only max elements in a queue as we move right, it wont matter if other elements are in left and not max
+        # in case the left index == max_element index we can just pop it fom queue
+        
+        n = len(runningCosts)
         left = 0
+        mono_dec_q = deque()
+        cur_cost = 0
+        ans = 0
+
         for i in range(n):
-            heapq.heappush(pq, -chargeTimes[i])
-            hm[chargeTimes[i]] += 1
-            cost += runningCosts[i]
-            if pq:
-                cur_budget = -pq[0] + (i-left+1)*cost
-            while left <= i and cur_budget > budget:
-                cost -= runningCosts[left]
-                time = chargeTimes[left]
-                hm[time] -= 1
-                while pq and hm[-pq[0]] == 0: # lazy deletion
-                    heapq.heappop(pq)
+            # pop all elements smaller than cur element to maintain mono decreasing q
+            while mono_dec_q and chargeTimes[mono_dec_q[-1]] <= chargeTimes[i]:
+                mono_dec_q.pop()
+            mono_dec_q.append(i)
+            cur_cost += runningCosts[i]
+            # shrink window to left
+            while mono_dec_q and (chargeTimes[mono_dec_q[0]] + (i-left+1)*cur_cost) > budget:
+                cur_cost -= runningCosts[left]
+                if mono_dec_q[0] == left:
+                    mono_dec_q.popleft()
                 left += 1
-                if pq and left <= i:
-                    cur_budget = -pq[0] + (i-left+1)*cost
-            if cur_budget <= budget:
-                ans = max(ans, (i-left+1))  
+        
+            ans = max(ans, (i-left+1))
         
         return ans
-
-
-                    
-
