ef92f535869fd5e79a72ac4b734bfeb47bb333b9
suyogk23 <suyogk2301@gmail.com>
2026-02-17 14:05:43 +0530

    Time Based Key-Value Store alternative solution

---
+++ Time Based Key-Value Store.py
@@ -0,0 +1,25 @@
+class TimeMap:
+    
+    def __init__(self):
+        self.hm = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None: #O(1)
+        self.hm[key].append([timestamp, value])
+
+    def get(self, key: str, timestamp: int) -> str: #O(log n)
+        # binary search the most recent timestamp 
+        values = self.hm[key]
+        n = len(values)
+        l, r = 0, n-1
+        ans = 
+        while l <= r:
+            m = (l + r)//2
+            if values[m][0] <= timestamp:
+                ans = values[m][1]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return ans  
+
+
